eef69fcb3008e31f1290fbb03064051eee219a3a
Alex Spiers <advspiers@gmail.com>
2022-01-12 13:42:41 +0000

    Add Procfile and Aptfile for heroku deployment

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 from ml import inference as infer
+import os
 
-
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    os.system("dvc remote add -d -f s3remote s3://census-fastapi")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    
 
 app = FastAPI()
 
@@ -45,41 +52,6 @@ class NewData(BaseModel):
                 }
         }
 
-# test_dict = {
-#   "age": 20,
-#   "workclass": "Self-emp-not-inc",
-#   "fnlgt": 205100,
-#   "education": "HS-grad",
-#   "education_num": 9,
-#   "marital_status": "Married-civ-spouse",
-#   "occupation": "Exec-managerial",
-#   "relationship": "Wife",
-#   "race": "White",
-#   "sex": "Female",
-#   "capital_gain": 0,
-#   "capital_loss": 0,
-#   "hours_per_week": 40,
-#   "native_country": "United-States"
-# }
-# test_dict = {key: [value] for key, value in test_dict.items()}
-# test_df = pd.DataFrame.from_dict(test_dict)
-
-# print(test_df)
-# test_df.columns = test_df.columns.str.replace("_", "-")
-# categorical_features = ["workclass",
-#         "education",
-#          "marital-status",
-#         "occupation",
-#         "relationship",
-#         "race",
-#         "sex",
-#         "native-country"
-#         ]
-
-
-# X_categorical = test_df[categorical_features].values
-# y_pred = infer.inference(test_df, output="string",label=None)
-# print(y_pred)
 
 @app.get("/")
 async def read_root():
