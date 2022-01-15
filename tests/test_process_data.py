5002e53521e9a5f1e98bd147547441f043650813
Alex Spiers <advspiers@gmail.com>
2022-01-15 20:29:13 +0000

    Move test_train_model.py data processing unit tests to test_process_data.py

--- tests/test_process_data.py
+++ tests/test_process_data.py
@@ -23,6 +23,13 @@ def processed_data():
     return X, y, encoder, lb
 
 
+@pytest.fixture
+def census_data():
+    df_path = "./data/census_cleaned.csv"
+    df = pd.read_csv(df_path)
+    yield df
+
+
 def test_y_binary(processed_data):
     X, y, _, _ = processed_data
     assert set(np.unique(y)) == {0, 1}
@@ -31,3 +38,44 @@ def test_y_binary(processed_data):
 def test_X_one_hot_enc(processed_data):
     X, _, _, _ = processed_data
     assert X.dtype == np.float64
+
+
+def test_import_type(census_data):
+    '''
+    Tests data imported correctly
+    '''
+    df = census_data
+    assert isinstance(df, pd.core.frame.DataFrame)
+
+
+def test_import_size(census_data):
+    '''
+    Tests data imported correctly
+    '''
+    df = census_data
+    assert df['salary'].size > 0
+    assert df.shape[1] > 0
+
+
+def test_feature_engineering_shape(processed_data):
+    X, y, _, _ = processed_data
+    X_train, X_test, y_train, y_test = process_data.perform_feature_engineering(
+        feature_set=X,
+        y=y
+    )
+    assert X_train.shape[0] > 0
+    assert X_test.shape[0] > 0
+    assert y_test.size > 0
+    assert y_train.size > 0
+
+
+def test_feature_engineering_size(processed_data, test_size=0.2):
+    X, y, _, _ = processed_data
+    _, X_test, _, y_test = process_data.perform_feature_engineering(
+        feature_set=X,
+        y=y
+    )
+    assert X_test.shape[0] - \
+        1 <= round(test_size * X.shape[0]) <= X_test.shape[0] + 1
+    assert y_test.size - \
+        1 <= round(test_size * X.shape[0]) <= y_test.size + 1
